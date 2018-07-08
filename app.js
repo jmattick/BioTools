d5c439153c1c6cadc476e9b4636c93c44d67215d
jmatty16 <jmatty1661@gmail.com>
2018-07-08 17:19:49 -0500

    add helix animation

---
+++ app.js
@@ -0,0 +1,65 @@
+//position helix container
+
+
+$( window ).on('load resize', function() {
+  //middle align helix and text in jumbotron
+let helix_height = $("#helix-container").height();
+let jumbotron_height = $(".jumbotron").height();
+let nav_height = $("nav").height();
+let landing_height = $(".landing-text").height();
+$("#helix-container").css("top", (jumbotron_height-nav_height)/2);
+$("#helix-container").css("margin-top",-helix_height/2);
+$(".landing-text").css("top", (jumbotron_height-nav_height)/2);
+$(".landing-text").css("margin-top",-landing_height/2);
+
+//if screen is too short for helix hide animation
+if(helix_height > jumbotron_height){
+  $("#helix-container").addClass('hidden');
+}
+
+
+
+
+});
+
+//get helix elements
+let helix1 = document.getElementsByClassName('base1');
+let helix2 = document.getElementsByClassName('base2');
+console.log(helix1);
+//make functions to add animation class
+var move_right_left = function(elem){
+  elem.classList.add('move-right-left');
+}
+var move_left_right = function(elem){
+  elem.classList.add('move-left-right');
+
+}
+
+//loop over elements to add animation with delay
+let delay = 0.5;
+
+$.each(helix1, function(){
+  var elem = $(this);
+  $(elem).css('animation-delay', delay+'s');
+  $(elem).addClass('move-right-left');
+  delay = delay + 0.5;
+});
+
+var delay2 = 0.5;
+$.each(helix2, function(){
+  var elem = $(this);
+  $(elem).css('animation-delay', delay2+'s');
+  $(elem).addClass('move-left-right');
+  delay2 = delay2 + 0.5;
+});
+
+
+
+
+// for(let i = 0; i < helix1.length; i++){
+//   // helix1[i].css('animation-delay', delay+'s');
+//   helix1[i].addClass('move-right-left');
+//   delay = delay + i ;
+  
+// };
+ 
